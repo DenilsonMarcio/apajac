bb3d1ffc31c2071b37f1ca45193b2eb6857016e4
Denilson Marcio <2212811@aluno.univesp.com>
2024-03-16 18:27:22 -0300

    first commit

---
+++ src/main/java/br/com/apajac/acolhimento/AcolhimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apajac.acolhimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcolhimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcolhimentoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/apajac/acolhimento/controllers/HelloWordController.java
@@ -0,0 +1,16 @@
+package br.com.apajac.acolhimento.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloWordController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Olá, Senhores aplicação Apajac já esta rodando!";
+    }
+
+}
