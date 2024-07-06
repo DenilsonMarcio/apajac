c5734c43505249566da07d3b7bde2af9a4fa685c
Caassapaba <jonathan.affini@gmai.com>
2024-07-05 22:50:22 -0300

    NT-9 BUSCA DE DOADOR

---
+++ src/main/java/com/apajac/acolhimento/mappers/DoadorMapper.java
@@ -0,0 +1,20 @@
+package com.apajac.acolhimento.mappers;
+
+import com.apajac.acolhimento.domain.dtos.DoadorDTO;
+import com.apajac.acolhimento.domain.entities.DoadorEntity;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DoadorMapper
+{
+
+    private final ModelMapper mapper;
+
+    public DoadorDTO convertEntityToDto(DoadorEntity Doador)
+    {
+        return mapper.map(Doador, DoadorDTO.class);
+    }
+}
---
+++ src/main/java/com/apajac/acolhimento/services/DoadorServiceImpl.java
@@ -0,0 +1,32 @@
+package com.apajac.acolhimento.services;
+
+import com.apajac.acolhimento.domain.entities.DoadorEntity;
+import com.apajac.acolhimento.exceptions.NotFoundException;
+import com.apajac.acolhimento.repositories.DoadorRepository;
+import com.apajac.acolhimento.services.interfaces.DoadorService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DoadorServiceImpl implements DoadorService
+{
+
+    private final DoadorRepository doadorRepository;
+
+    @Override
+    public DoadorEntity buscarDoadorPorId(Long id)
+    {
+        Optional<DoadorEntity> doadorOpt = doadorRepository.findById(id);
+        if (doadorOpt.isEmpty())
+        {
+            throw new NotFoundException("Doador não encontrado.");
+        }
+        return doadorOpt.get();
+    }
+}
