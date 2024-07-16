19064e31a8384487d4a12b1a866d9c2df299d72c
Caassapaba <jonathan.affini@gmai.com>
2024-07-16 11:36:45 -0300

    NT-9 BUSCA DE DOADORES, Remoção de anotação de tratamento de dados remanescente de revisão anterior, inclusão de anotação de enumeração.

--- src/main/java/com/apajac/acolhimento/services/DoadorServiceImpl.java
+++ src/main/java/com/apajac/acolhimento/services/DoadorServiceImpl.java
@@ -7,13 +7,11 @@ import com.apajac.acolhimento.services.interfaces.DoadorService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class DoadorServiceImpl implements DoadorService
 {
 
