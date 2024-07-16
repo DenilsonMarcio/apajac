e4a1f7d9bb7b1a16325d7d27989002c74a2c7b9e
Caassapaba <jonathan.affini@gmai.com>
2024-07-16 11:25:58 -0300

    NT-6 LISTA DE DOADORES, Remoção de anotação de tratamento de dados remanescente de revisão anterior, inclusão de anotação de enumeração.

--- src/main/java/com/apajac/acolhimento/services/DoadorServiceImpl.java
+++ src/main/java/com/apajac/acolhimento/services/DoadorServiceImpl.java
@@ -7,11 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class DoadorServiceImpl implements DoadorService
 {
 
