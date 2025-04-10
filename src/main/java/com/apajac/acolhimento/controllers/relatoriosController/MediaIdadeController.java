1b616a6f209fec1be8950c328e1d37a68ebd3f72
Leandro Alves <leandro_alvesbr@outlook.com>
2025-04-10 18:49:10 -0300

    [fix] nomeclatura MediaidadeController -> MediaIdadeController

--- src/main/java/com/apajac/acolhimento/controllers/relatoriosController/MediaIdadeController.java
+++ src/main/java/com/apajac/acolhimento/controllers/relatoriosController/MediaIdadeController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/relatorio")
-public class MediaidadeController {
+public class MediaIdadeController {
 
     private final RelatoriosService relatoriosService;
 
